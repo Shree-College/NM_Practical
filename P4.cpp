0b0d1466f0bea3bf2673f8859f0e5bdb4ddeb7aa
Shree <2022bec066@sggs.ac.in>
2023-09-05 13:45:08 +0530

    P4

---
+++ P4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+#define F(x) ((x*x*x)-(3*x)-5)
+#define G(x) ((3*x*x)-3)
+int main(){
+     
+float x0,x1,x2,f0,f1;
+int i=1;
+cout<<"Enter initial Guess\n";
+cin>>x0;
+
+f0=F(x0);
+f1=G(x0);
+
+do{
+    x1 = x0 - (f0/f1);
+    f0=F(x1);
+    f1=G(x1);
+    cout<<"x"<<i<<"="<<x1<<"\tF(x"<<i<<")="<<f0<<"\n";
+    x0=x1;
+    i++;
+}while(fabs(F(x1))>0.00001);
+
+cout<<"\nThe Root of equation is - "<<x1;
+
+    return 0;
+}
